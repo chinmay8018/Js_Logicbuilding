1e11d2297f3fc96f7d1f59e1ae629f9ab103395c
ChinmaY <chinmayjagdevd@gmail.com>
2026-03-12 11:18:26 +0530

    array all method

---
+++ array.js
@@ -0,0 +1,12 @@
+//An Array is a data structure used to store multiple values in a single variable.
+
+let arr = [1,2,3,4,5,6,7,8];
+
+console.log(arr[0]);
+console.log(arr[1]);
+
+//Method 2 – Array Constructor
+let numbers = new Array(1,1,2,3,4,5,6,);
+
+console.log(numbers[3]);
+console.log(numbers[6]);
