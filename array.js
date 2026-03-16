adf1962f86ca86eddf7d3e2c7732fc1c80636162
chinmay <chinmayjagdevd@.com>
2026-03-16 22:53:57 +0530

    addition in the array methods

---
+++ array.js
@@ -0,0 +1,24 @@
+//An Array is a data structure used to store multiple values in a single variable.
+
+let arr = [1,2,3,4,5,6,7,8];
+
+console.log(arr[0]);
+console.log(arr[1]);
+
+//Method 2 – Array Constructor
+let numbers = new Array(1,1,2,3,4,5,6,);
+
+console.log(numbers[3]);
+console.log(numbers[6]);
+
+//Creating Array with Size
+let array = new Number(5);
+ array[0] = 10;
+ array[1] = 20;
+ array[2] = 30;
+ array[3] = 40;
+ array[4] = 50;
+ 
+
+console.log(array);
+console.log(arr.length);
