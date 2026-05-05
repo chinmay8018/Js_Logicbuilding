f69d17feecb01dca98163590d06a7f0a8bcf8497
chinmay <chinmayjagdevd@.com>
2026-05-05 22:40:35 +0530

    acessss

---
+++ oops/acsess_modifier.js
@@ -0,0 +1,16 @@
+class Teacher {
+    constructor(name){
+        this.name = name; //public
+
+    }
+}
+
+let t1 =new Teacher("chinmay");
+console.log(t1.name); //
+
+
+class Teacher2{
+    #salary //private
+
+     
+}
