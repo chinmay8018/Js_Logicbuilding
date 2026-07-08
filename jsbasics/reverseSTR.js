fa60ea35b3b85a867e02ab688dc09642725d7d4b
chinmay <chinmayjagdevd@.com>
2026-07-08 22:44:41 +0530

    reversed a number

---
+++ jsbasics/reverseSTR.js
@@ -0,0 +1,10 @@
+function reverseaString(str){
+    let reversed ="";
+
+    for (let i=str.length-1;i>=0;i--){
+        reversed += str[i];
+
+    }
+    return reversed;
+}
+console.log(reverseaString("12345"));
